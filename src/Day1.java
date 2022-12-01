ba4a5d5af5aa8c8c60b193209b8106f26422e760
Liam Mattsson <liammattsson@hotmail.com>
2022-12-01 10:43:21 +0100

    Removed "elfNumber" and imports for day 1 solutions.

--- src/Day1.java
+++ src/Day1.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.Buffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,8 +6,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Day1 {
 
