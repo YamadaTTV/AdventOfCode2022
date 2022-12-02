ca78764fba619b40517460dab0763cff63a4adbd
Liam Mattsson <liammattsson@hotmail.com>
2022-12-02 15:07:50 +0100

    Day 2 part 2 solution (with comments)

--- src/Day2.java
+++ src/Day2.java
@@ -49,9 +49,66 @@ public class Day2 {
         System.out.println("Answer: My total score is " + total_score);
     }
 
+    public static void part2(List<String> list){
+        int total_score = 0;
+        list = Collections.emptyList();
+        try {
+            list = Files.readAllLines(Paths.get("C:\\Users\\liamm\\Documents\\AdventOfCode\\src\\resources\\input2.txt"), StandardCharsets.UTF_8);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        System.out.println(list.toString());
+        for(int i = 0; i < list.size(); i++){
+            String mine = list.get(i).substring(2);
+            String ai = list.get(i).substring(0,1);
+            if(mine.equals("X")){   // You need to lose!
+                // Since we lose, no points are added...
+                if(ai.equals("A")){ // AI chooses Rock
+                    // So we need to choose scissors
+                    total_score = total_score + 3;
+                } else if(ai.equals("B")){  // AI chooses paper
+                    // So we need to choose rock
+                    total_score = total_score + 1;
+                } else if(ai.equals("C")){  // AI chooses scissors
+                    // So we need to choose paper
+                    total_score = total_score + 2;
+                }
+            } else if(mine.equals("Y")){    // You need to end the round in a draw!
+                // Since we get a draw, we add 3 points
+                total_score = total_score + 3;
+                if(ai.equals("A")){     // AI chooses rock
+                    // So we need to choose rock
+                    total_score = total_score + 1;
+                } else if(ai.equals("B")){  // AI chooses paper
+                    // So we need to choose paper
+                    total_score = total_score + 2;
+                } else if(ai.equals("C")){  // AI chooses scissors
+                    // So we need to choose scissors
+                    total_score = total_score + 3;
+                }
+            } else if(mine.equals("Z")){    // You need to win!
+                // Since we get a win, we add 6 points
+                total_score = total_score + 6;
+                if(ai.equals("A")){     // AI chooses rock
+                    // So we need to choose paper
+                    total_score = total_score + 2;
+                } else if(ai.equals("B")){  // AI chooses paper
+                    // So we need to choose scissors
+                    total_score = total_score + 3;
+                } else if(ai.equals("C")){  // AI chooses scissors
+                    // So we need to choose rock
+                    total_score = total_score + 1;
+                }
+            }
+        }
+        System.out.println("Answer: My total score is: " + total_score);
+    }
+
     public static void main(String[] args){
         List<String> test = new ArrayList<>();
         /* Part 1 */
         part1(test);
+        /* Part 2 */
+        part2(test);
     }
 }
