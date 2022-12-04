22010a2513ffb19d1c025515b4b4f5c72b659647
Liam Mattsson <liammattsson@hotmail.com>
2022-12-04 01:09:14 +0100

    Day 3 part 1 solutions, might improve this solution later...

--- src/Day3.java
+++ src/Day3.java
@@ -2,31 +2,46 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Day3 {
 
+    public static String substring(int a, int b, String temp){
+        String res = "";
+        for(int i = a; i < b; i++){
+            char ch1 = temp.charAt(i);
+            res = res + ch1;
+        }
+        return res;
+    }
+
+
     public static void part1(List<String> list){
+        int sumOfPriorities = 0;
         list = Collections.emptyList();
         try{
             list = Files.readAllLines(Paths.get("C:\\Users\\liamm\\Documents\\AdventOfCode\\src\\resources\\input3.txt"), StandardCharsets.UTF_8);
         } catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println(list);
         for(int i = 0; i < list.size(); i++){
-            String firstCompartment = list.get(i).substring(0,13);
-            String secondCompartment = list.get(i).substring(13);
+            String firstCompartment = substring(0, (list.get(i).length()/2), list.get(i));
+            String secondCompartment = substring((list.get(i).length()/2), list.get(i).length(), list.get(i));
+            int duplicate = 0;
             for(int j = 0; j < firstCompartment.length(); j++){
                 for(int k = 0; k < secondCompartment.length(); k++){
                     if(firstCompartment.charAt(j) == secondCompartment.charAt(k)){
-                        // Both compartments share an item type
+                        duplicate = secondCompartment.charAt(k);
                     }
                 }
             }
+            if(Character.isUpperCase(duplicate)){
+                sumOfPriorities += duplicate - 38;
+            } else {
+                sumOfPriorities += duplicate - 96;
+            }
         }
+        System.out.println("Answer: Sum of priorities is " + sumOfPriorities);
     }
 
     public static void main(String[] args){
