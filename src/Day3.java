b806734538780bddcffee91b88bdb7eb8fbb098f
Liam Mattsson <liammattsson@hotmail.com>
2022-12-05 15:49:58 +0100

    Day 3 part 2 solutions.

--- src/Day3.java
+++ src/Day3.java
@@ -44,8 +44,45 @@ public class Day3 {
         System.out.println("Answer: Sum of priorities is " + sumOfPriorities);
     }
 
+    public static void part2(List<String> list){
+        int sumOfPriorities = 0;
+        list = Collections.emptyList();
+        try{
+            list = Files.readAllLines(Paths.get("C:\\Users\\liamm\\Documents\\AdventOfCode\\src\\resources\\input3.txt"), StandardCharsets.UTF_8);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        // We want to find one common character between three lines (a.k.a one group)
+        char isCommon = ' ';
+        for(int i = 2; i < list.size(); i = i + 3){
+            char[] elf1 = list.get(i-2).toCharArray();
+            char[] elf2 = list.get(i-1).toCharArray();
+            char[] elf3 = list.get(i).toCharArray();
+            for(char c: elf1){
+                for(char c2: elf2){
+                    if(c == c2){
+                        for(char c3: elf3){
+                            if(c3 == c2){
+                                isCommon = c3;
+                            }
+                        }
+                    }
+                }
+            }
+            if(Character.isUpperCase(isCommon)){
+                sumOfPriorities += ((int)isCommon) - 38;
+            } else {
+                sumOfPriorities += ((int)isCommon) - 96;
+            }
+        }
+        System.out.println("Answer: Sum of priorities is " + sumOfPriorities);
+    }
+
     public static void main(String[] args){
         List<String> test = new ArrayList<>();
+        /* Part 1 */
         part1(test);
+        /* Part 2 */
+        part2(test);
     }
 }
