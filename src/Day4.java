4232ee7e3b0e709d94cc942e367b2136d3806341
YamadaTTV <92035299+YamadaTTV@users.noreply.github.com>
2022-12-06 01:04:43 +0100

    Day 4 part 2 solution.

--- src/Day4.java
+++ src/Day4.java
@@ -33,6 +33,31 @@ public class Day4 {
         System.out.println("Answer: " + containTheOther);
     }
 
+    public static void part2(List<String> list){
+        list = Collections.emptyList();
+        try{
+            list = Files.readAllLines(Paths.get("src\\resources\\input4.txt"));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        int overlaps = 0;
+        for(int i = 0; i < list.size(); i++){
+            String[] pairSplit = list.get(i).split(",");
+            String[] firstPair = pairSplit[0].split("-");
+            String[] secondPair = pairSplit[1].split("-");
+
+            int beginFirstInterval = Integer.parseInt(firstPair[0]);
+            int endFirstInterval = Integer.parseInt(firstPair[1]);
+            int beginLastInterval = Integer.parseInt(secondPair[0]);
+            int endLastInterval = Integer.parseInt(secondPair[1]);
+
+            if(Overlaps(beginFirstInterval,endFirstInterval,beginLastInterval,endLastInterval) || Overlaps(beginLastInterval,endLastInterval,beginFirstInterval,endFirstInterval)){
+                overlaps = overlaps + 1;
+            }
+        }
+        System.out.println("Answer: The number of overlaps is: " + overlaps);
+    }
+
 
     private static boolean DoesContain(int x1, int x2, int y1, int y2){
         if(y1 <= x1 && x2 <= y2){
@@ -41,10 +66,19 @@ public class Day4 {
         return false;
     }
 
+    public static boolean Overlaps(int x1, int x2, int y1, int y2){
+        if((x1 >= y1 && x1 <= y2) || (x2 >= y1 && x2 <= y2)){
+            return true;
+        }
+        return false;
+    }
 
 
     public static void main(String[] args){
         List<String> test = new ArrayList<>();
+        /* Part 1 */
         part1(test);
+        /* Part 2 */
+        part2(test);
     }
 }
