ee660568e055f414a327224551476d6354bf37b9
YamadaTTV <92035299+YamadaTTV@users.noreply.github.com>
2022-12-11 15:00:51 +0100

    Day 8 part 2 solution.

--- src/Day8.java
+++ src/Day8.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,7 +16,6 @@ public class Day8 {
         }
         String[] rows = list.toArray(new String[0]);
         int[][] arr2D = new int[rows.length][rows.length];
-        System.out.println(Arrays.toString(rows));
         for(int i = 0; i < rows.length; i++){
             for(int j = 0; j < rows.length; j++){
                 arr2D[i][j] = Integer.parseInt(String.valueOf(rows[i].charAt(j)));
@@ -30,11 +28,75 @@ public class Day8 {
                 }
             }
         }
-        System.out.println(visibleTrees);
+        System.out.println("Answer: "+ visibleTrees + " trees are visible from outside the grid");
     }
 
     public static void part2(){
+        int highestScenicScore = 0;
+        int currentScenicScore;
+        List<String> list = Collections.emptyList();
+        try{
+            list = Files.readAllLines(Paths.get("src\\resources\\input8.txt"));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        String[] rows = list.toArray(new String[0]);
+        int[][] arr2D = new int[rows.length][rows.length];
+        for(int row = 0; row < rows.length; row++){
+            for(int col = 0; col < rows.length; col++){
+                arr2D[row][col] = Integer.parseInt(String.valueOf(rows[row].charAt(col)));
+            }
+        }
+        for(int row = 0; row < arr2D[0].length; row++){
+            for(int col = 0; col < arr2D[1].length; col++){
+                currentScenicScore = calcScenicScore(arr2D,row,col);
+                if(highestScenicScore < currentScenicScore){
+                    highestScenicScore = currentScenicScore;
+                }
+            }
+        }
+        System.out.println("Answer: The highest scenic score possible is " + highestScenicScore);
+    }
 
+    public static int calcScenicScore(int[][] input, int row, int col){
+        int currentTree = input[row][col];
+        int treesSeenUp = 0;
+        int treesSeenLeft = 0;
+        int treesSeenDown = 0;
+        int treesSeenRight = 0;
+        // Looking up
+        for(int r = row-1; r >= 0; r--){
+            treesSeenUp++;
+            int nextTree = input[r][col];
+            if(nextTree >= currentTree){
+                break;
+            }
+        }
+        // Looking left
+        for(int c = col-1; c >= 0; c--){
+            treesSeenLeft++;
+            int nextTree = input[row][c];
+            if(nextTree >= currentTree){
+                break;
+            }
+        }
+        // Looking down
+        for(int r = row+1; r < input[0].length; r++){
+            treesSeenDown++;
+            int nextTree = input[r][col];
+            if(nextTree >= currentTree){
+                break;
+            }
+        }
+        // Looking right
+        for(int c = col+1; c < input[1].length; c++){
+            treesSeenRight++;
+            int nextTree = input[row][c];
+            if(nextTree >= currentTree){
+                break;
+            }
+        }
+        return treesSeenUp * treesSeenLeft * treesSeenDown * treesSeenRight;
     }
 
     public static boolean isVisible(int[][] input, int row, int col){
@@ -78,5 +140,6 @@ public class Day8 {
 
     public static void main(String[] args){
         part1();
+        part2();
     }
 }
